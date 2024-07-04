468d36660075a1a3eca640b3f1ef8b8265c7694f
Lee Easson <leasson@nevada.unr.edu>
2024-07-03 20:02:47 -0700

    Add stylesheets for titles and buttons, complete with views

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import React from 'react';
 
 export default function RootLayout() {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import { Button, Alert, StyleSheet, Image, Platform, Text, SafeAreaView, View } from "react-native";
 
-export default function Index() {
+import { HelloWave } from '@/components/HelloWave';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+const Separator = () => <View style={styles.separator} />;
+
+export default function TitleMenu() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.titleContainer}>
+      <View>
+        <ThemedText style={styles.titleText}>YEAR GUESSER</ThemedText>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button title="PLAY"      onPress={() => Alert.alert('Play Button pressed')}/>
+        <Button title="TUTORIAL"  color="#f194ff"  onPress={() => Alert.alert('Tutorial Button pressed')}/>
+        <Button title="SETTINGS"  color="#841584"  onPress={() => Alert.alert('Settings Button pressed')}/>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: "center",
+    gap: 8,
+    flex: 1,
+  },
+  baseText: {
+    fontFamily: 'SpaceMono-Regular',
+  },
+  titleText: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  buttonContainer: {
+    marginVertical: 8,
+  }
+});
